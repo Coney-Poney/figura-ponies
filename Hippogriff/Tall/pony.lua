b1e70f0a3afcc022951fbb1fd2d821d9eae86b9e
winterClover <66449394+winterClover@users.noreply.github.com>
2023-01-29 15:37:46 -0600

    Parrots are now visible

--- Hippogriff/Tall/pony.lua
+++ Hippogriff/Tall/pony.lua
@@ -1,6 +1,7 @@
 vanilla_model.ALL:setVisible(false)
 vanilla_model.HELD_ITEMS:setVisible(true)
 vanilla_model.HELMET_ITEM:setVisible(true)
+vanilla_model.PARROTS:setVisible(true)
 
 -- pony stuff
 Magic = false
