6c72e5c7715835b7c97636c770eadc024d4bfa4c
winterClover <66449394+winterClover@users.noreply.github.com>
2023-02-23 21:49:55 -0600

    KCM Update

--- Ponies/Large/KattArmorCustomModel.lua
+++ Ponies/Large/KattArmorCustomModel.lua
@@ -22,8 +22,8 @@ local parts = {
 
 local API = {}
 
----Adds the given modelParts to be enabled when the given armorID wears the material `materialID``
----
+---Adds the given modelParts to be enabled when the given armorID wears the material `materialID`<br>
+---A `materialID` of `false` will set ModelParts that will be invisible when there is any armor set equipped in the given slot, otherwise visible.
 ---@param armorID KattArmor.ArmorPartID
 ---@param materialID KattArmor.ArmorMaterialID | false
 ---@param ... ModelPart
@@ -31,7 +31,7 @@ function API.addModelPartToMaterial(armorID, materialID, ...)
   if not parts[armorID] then error(("Expected valid ArmorPartID, recieved \"%s\""):format(armorID), 2) end
   if not parts[armorID][materialID] then parts[armorID][materialID] = {} end
   local partList = parts[armorID][materialID]
-  for index, modelPart in ipairs({...}) do
+  for index, modelPart in ipairs({ ... }) do
     if type(modelPart) ~= "ModelPart" then error(("Invalid type for varargs index %i. Expected a <ModelPart>, recieved <%s> %s"):format(index, type(modelPart), tostring(modelPart)), 2) end
     modelPart:setVisible(false)
     table.insert(partList, modelPart)
@@ -39,14 +39,14 @@ function API.addModelPartToMaterial(armorID, materialID, ...)
   ItemChangeAPI.forceUpdate(ItemChangeAPI.ItemSlots[armorID])
 end
 
-require((...):gsub("(.)$", "%1.") .. "KattArmorAPI").onChange:register(function(arg, armorID, material, _, prevMaterial, _)
+require((...):gsub("(.)$", "%1.") .. "KattArmorAPI").onRender:register(function(arg, armorID, material, _, prevMaterial, _)
   if parts[armorID][prevMaterial or false] then
     for _, modelPart in ipairs(parts[armorID][prevMaterial or false]) do
       modelPart:setVisible(false)
     end
   end
   if parts[armorID][material or false] then
-    local visible = arg.visible and nil
+    local visible = material and (arg.visible and nil)
     for _, modelPart in ipairs(parts[armorID][material or false]) do
       modelPart:setVisible(visible)
     end
